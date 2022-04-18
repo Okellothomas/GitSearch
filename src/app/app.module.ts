9c28cd9b1b70870d832050d4d7c834e0572c8103
Okellothomas <thomasolal33@gmail.com>
2022-04-18 18:10:24 +0300

    Add myrepos and myporfolio components to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TimeCountPipe } from './time-count.pipe';
+import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
+import { MyReposComponent } from './my-repos/my-repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TimeCountPipe
+    TimeCountPipe,
+    MyPortfolioComponent,
+    MyReposComponent
   ],
   imports: [
     BrowserModule,
