dd0726551dff13a20b0cef1eb12e51c703099d29
Okellothomas <thomasolal33@gmail.com>
2022-04-19 17:27:08 +0300

    add customization to myresoitories

--- src/app/my-portfolio/my-portfolio.component.ts
+++ src/app/my-portfolio/my-portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GitservicesService } from '../gitservices.service';
 
 @Component({
   selector: 'app-my-portfolio',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyPortfolioComponent implements OnInit {
 
-  constructor() { }
+  public userName!:string;
+  public githubprofile!:any;
+  public githubrepos!: any[];
+  public errorMessage!:string;
+
+  constructor(private myGitSearch:GitservicesService) { 
+    this.userName = 'Okellothomas'
+  }
+
+  public searchName(){
+
+    // to get the github profile
+    // this.githubserchSerive.userProfileInfo(this.userName).subscribe((data)=>{
+    //   this.githubprofile = data;
+    // },(error)=>{
+    //   this.errorMessage = error;
+    // });
+
+    this.myGitSearch.myProfileInfo().subscribe((data)=>{
+      this.githubprofile = data;
+    },(error)=>{
+      this.errorMessage = error;
+    });
+
+    // to the the github repos. 
+    this.myGitSearch.myreposInfo().subscribe((data)=>{
+      this.githubrepos= data;
+    },(error)=>{
+      this.errorMessage = error;
+    });
+    
+  }
 
   ngOnInit(): void {
   }
--- src/app/my-repos/my-repos.component.ts
+++ src/app/my-repos/my-repos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GitservicesService } from '../gitservices.service';
 
 @Component({
   selector: 'app-my-repos',
@@ -7,7 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyReposComponent implements OnInit {
 
-  constructor() { }
+  public userName!:string;
+  public githubprofile!:any;
+  public githubrepos!: any[];
+  public errorMessage!:string;
+
+  constructor(private myGitSearch:GitservicesService) { 
+    this.userName = 'Okellothomas'
+  }
+
+  public searchName(){
+
+    // to get the github profile
+    // this.githubserchSerive.userProfileInfo(this.userName).subscribe((data)=>{
+    //   this.githubprofile = data;
+    // },(error)=>{
+    //   this.errorMessage = error;
+    // });
+
+    this.myGitSearch.myProfileInfo().subscribe((data)=>{
+      this.githubprofile = data;
+    },(error)=>{
+      this.errorMessage = error;
+    });
+
+    // to the the github repos. 
+    this.myGitSearch.myreposInfo().subscribe((data)=>{
+      this.githubrepos= data;
+    },(error)=>{
+      this.errorMessage = error;
+    });
+    
+  }
+
 
   ngOnInit(): void {
   }
